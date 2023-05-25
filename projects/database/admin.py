48b67bfb5eabb47510c5b08cb34a8ddcc49712e5
theprogrammer0 <110111962+theprogrammer0@users.noreply.github.com>
2023-05-25 11:01:21 +0100

    Codes for the Admin.py and Models.py

--- projects/database/admin.py
+++ projects/database/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Employee
 
-# Register your models here.
+
+admin.site.site_header = 'Admin Tutorial Dashboard'
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('role_name', 'first_name','last_name', 'email','password','status', 'position', 'funded_by', 'annual_salary', 'annual_salary', 'is_admin', )
+
+
+
+admin.site.register(Employee, EmployeeAdmin)
--- projects/database/models.py
+++ projects/database/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    employee_id = models.CharField(max_length=20, primary_key=True,)
+    role_name = models.CharField(max_length=45, )
+    first_name = models.CharField(max_length=45, )
+    last_name = models.CharField(max_length=45,)
+    email = models.CharField(max_length=40,)
+    password = models.CharField(max_length=20,)
+    status = models.CharField(max_length=8, choices=[('Active', 'Active'), ('Inactive', 'Inactive')], )
+    position = models.CharField(max_length=45,)
+    funded_by = models.CharField(max_length=45,)
+    annual_salary = models.DecimalField(max_digits=10, decimal_places=0,)
+    is_admin = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')], )
+
+
+    def __str__(self):
+        return self.employee_id
+
+    def body_preview(self):
+        return self.body[:50]
+
+
