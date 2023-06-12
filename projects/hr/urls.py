50b9d23ff336dcadec3f2322ab31cabc09627870
Marc <marcmcdonnelljnr@gmail.com>
2023-06-12 10:10:38 +0100

    updating modified files
    html files

--- projects/hr/urls.py
+++ projects/hr/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from database import views
+from django.contrib.auth.views import(PasswordResetView,PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,12 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('signout/',views.signout, name='signout'),
     path('profile/',views.profile, name='profile'),
+
+    path('password-reset/', PasswordResetView.as_view(template_name='password_reset.html'),name='password-reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+         
 
 
     #region Admin Section
@@ -70,6 +77,7 @@ urlpatterns = [
          views.evaluationpart1, name='evaluationpart1'),
     path('profile/staff/evaluationpart1/evaluationpart2/',
           views.evaluationpart2, name='evaluationpart2'),
+    
 
     #endregion
 ]
